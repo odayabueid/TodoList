ec87d6dc2ee8895f7111c947b916fe21905e5254
Oday Abueid <odayabueid1992m@gmail.com>
2019-08-09 12:44:53 +0300

    styling begining

--- my-app/src/components/SignIn.js
+++ my-app/src/components/SignIn.js
@@ -1,6 +1,28 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router'
 import Todos from "./Todos"
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import { AccessAlarm, ThreeDRotation } from '@material-ui/icons';
+import styles from './mystyle.module.css'; 
+import { spacing } from '@material-ui/system';
+
+
+
+
+
+
 class SignIn extends React.Component{
     constructor(props){
         super(props);
@@ -26,6 +48,7 @@ class SignIn extends React.Component{
 
 
   server(e){
+    e.preventDefault();
     var that = this;
     fetch("signin/",{
         method:"POST",
@@ -79,14 +102,88 @@ renderRedirect = () =>{
   }
 
 
+  
   render(){
+    
     return(
+        
         <div>
-            <input type="text" onChange={this.user.bind(this)} placeholder="username"></input>
+            <Grid container component="main" className={styles.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={styles.image} />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <div className={styles.paper}>
+          <Avatar className={styles.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <form className={styles.form} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="user"
+              label="User Name"
+              name="user"
+              autoComplete="user"
+              autoFocus
+              onChange={this.user.bind(this)}
+              
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              onChange={this.pass.bind(this) }
+              
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              style={{margin:'24px 0px 16px'}}
+              onClick={this.server.bind(this)}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2" onClick={this.SignUp}>
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+            <Box mt={5}>
+            </Box>
+          </form>
+        </div>
+      </Grid>
+    </Grid>
+    {this.renderRedirect()}
+            {/* <input type="text" onChange={this.user.bind(this)} placeholder="username"></input>
             <input type="text" onChange={this.pass.bind(this) }placeholder="password"></input>
             <button onClick={this.server.bind(this)}>Sign In</button>
             <p onClick={this.SignUp}>you don't have an account? please signup</p>
-            {this.renderRedirect()}
+            {this.renderRedirect()} */}
           
         </div>
     )}
--- my-app/src/components/SignUp.js
+++ my-app/src/components/SignUp.js
@@ -1,5 +1,20 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router'
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import styles from './mystyle.module.css'; 
+
 
 class SignUp extends React.Component{
     constructor(props){
@@ -7,7 +22,8 @@ class SignUp extends React.Component{
         this.state = {            
             password:'',
             username:"",
-            redirect:false
+            redirect:false,
+            login:false
         }
   }
 
@@ -22,7 +38,8 @@ pass =(event)=>{
         password:event.target.value
     },()=>console.log(this.state.password))
 }
-server=()=>{
+server=(e)=>{
+    e.preventDefault();
     var that = this;
     fetch("signup/",{
         method:"POST",
@@ -51,6 +68,12 @@ server=()=>{
       });
 }
 
+login=()=>{
+    this.setState({
+        login:true
+    })
+}
+
 renderRedirect = () =>{
     if(this.state.redirect){
       alert("regester Success")
@@ -58,15 +81,104 @@ renderRedirect = () =>{
         pathname:"/signin"
         }}/>
     }
+    if(this.state.login){
+        return <Redirect to = {{
+          pathname:"/signin"
+          }}/>
+      }
   }
 
   render(){
     return(
         <div>
+
+
+<Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={styles.paper1}>
+        <Avatar className={styles.avatar1}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <form className={styles.form1} noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12} >
+              <TextField
+                autoComplete="uname"
+                name="userName"
+                variant="outlined"
+                required
+                fullWidth
+                id="usename"
+                label="User Name"
+                autoFocus
+                onChange={this.user} 
+              />
+            </Grid>
+    
+            {/* <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+              />
+            </Grid> */}
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                onChange={this.pass}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <FormControlLabel
+                control={<Checkbox value="allowExtraEmails" color="primary" />}
+                label="I want to receive inspiration, marketing promotions and updates via email."
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            style={{ margin:"24px 0px 16px" }}
+            onClick={this.server}
+          >
+            Sign Up
+          </Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Link href="#" variant="body2" onClick={this.login}>
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={5}>
+        
+      </Box>
+    </Container>
+
+
+
             {this.renderRedirect()}
-            <input type="text" onChange={this.user} placeholder="your name"></input>
+            {/* <input type="text" onChange={this.user} placeholder="your name"></input>
             <input type="text" onChange={this.pass} placeholder="your pass"></input>
-            <button onClick={this.server}>signup</button>
+            <button onClick={this.server}>signup</button> */}
         </div>
     )}
 
